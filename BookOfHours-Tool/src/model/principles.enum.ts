312aed8fd84510480f0c171d403d1d97da3bc7a4
ryancwilliams <rcw4rcw@gmail.com>
2024-07-13 16:41:16 -0400

    Added Mystery

--- BookOfHours-Tool/src/model/principles.enum.ts
+++ BookOfHours-Tool/src/model/principles.enum.ts
@@ -67,3 +67,51 @@ export class Principle {
 	}
 
 }
+
+/**
+ * A specific Mystery
+ */
+export class Mystery {
+	private static readonly SYMBOL_PATH_PREFIX = "/images/mystery/mystery."
+
+	/**
+	 * Map of EPrinciple to Mystery
+	 */
+	public static readonly MYSTERIES = new Map<EPrinciple, Mystery>([
+		[EPrinciple.EDGE, new Mystery(EPrinciple.EDGE, "Edge", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "edge.png")],
+		[EPrinciple.FORGE, new Mystery(EPrinciple.FORGE, "Forge", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "forge.png")],
+		[EPrinciple.GRAIL, new Mystery(EPrinciple.GRAIL, "Grail", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "grail.png")],
+		[EPrinciple.HEART, new Mystery(EPrinciple.HEART, "Heart", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "heart.png")],
+		[EPrinciple.KNOCK, new Mystery(EPrinciple.KNOCK, "Knock", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "knock.png")],
+		[EPrinciple.LANTERN, new Mystery(EPrinciple.LANTERN, "Lantern", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "lantern.png")],
+		[EPrinciple.MOON, new Mystery(EPrinciple.MOON, "Moon", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "moom.png")],
+		[EPrinciple.MOTH, new Mystery(EPrinciple.MOTH, "Moth", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "moth.png")],
+		[EPrinciple.NECTAR, new Mystery(EPrinciple.NECTAR, "Nectar", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "nectar.png")],
+		[EPrinciple.ROSE, new Mystery(EPrinciple.ROSE, "Rose", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "rose.png")],
+		[EPrinciple.SCALE, new Mystery(EPrinciple.SCALE, "Scale", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "scale.png")],
+		[EPrinciple.SKY, new Mystery(EPrinciple.SKY, "Sky", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "sky.png")],
+		[EPrinciple.WINTER, new Mystery(EPrinciple.WINTER, "Winter", new RenderableSymbol(this.SYMBOL_PATH_PREFIX + "winter.png")]
+	]);
+
+	/**
+	 * The enum object for this Mystery.
+	 */
+	public readonly principle: EPrinciple;
+
+	/**
+	 * The name of the Mystery.
+	 */
+	public readonly name: string;
+
+	/**
+	 * The path to the symbol for the Mystery.
+	 */
+	public readonly symbolPath: RenderableSymbol;
+
+	private constructor(principle: EPrinciple, name: string, symbolPath: RenderableSymbol){
+		this.principle = principle;
+		this.name = name;
+		this.symbolPath = symbolPath;
+	}
+
+}
