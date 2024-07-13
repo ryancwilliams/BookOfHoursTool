e1fe7650c02b84cbb70504c241035a774a7f48be
ryancwilliams <rcw4rcw@gmail.com>
2024-07-13 11:40:24 -0400

    Updated Language

--- BookOfHours-Tool/src/model/language.enum.ts
+++ BookOfHours-Tool/src/model/language.enum.ts
@@ -28,21 +28,29 @@ export enum ELanguage {
  */
 export class Language {
 	private static readonly SYMBOL_PATH_PREFIX = "/images/language/"
-	public static readonly ARAMAIC = new Language(ELanguage.ARAMAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.aramaic.png"));
-	public static readonly CRACKTRACK = new Language(ELanguage.CRACKTRACK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.cracktrack.png"));
-	public static readonly ERICAPAEAN = new Language(ELanguage.ERICAPAEAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ericapaean.png"));
-	public static readonly FUCINE = new Language(ELanguage.FUCINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.fucine.png"));
-	public static readonly GREEK = new Language(ELanguage.GREEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.greek.png"));
-	public static readonly HENAVEK = new Language(ELanguage.HENAVEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.henavek.png"));
-	public static readonly HYKSOS = new Language(ELanguage.HYKSOS, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.hyksos.png"));
-	public static readonly KILLASIMI = new Language(ELanguage.KILLASIMI, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.killasimi.png"));
-	public static readonly LATIN = new Language(ELanguage.LATIN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.latin.png"));
-	public static readonly MANDAIC = new Language(ELanguage.MANDAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.mandaic.png"));
-	public static readonly PHRYGIAN = new Language(ELanguage.PHRYGIAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.phrygian.png"));
-	public static readonly RAMSUND = new Language(ELanguage.RAMSUND, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ramsund.png"));
-	public static readonly SABAZINE = new Language(ELanguage.SABAZINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sabazine.png"));
-	public static readonly SANSKRIT = new Language(ELanguage.SANSKRIT, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sanskrit.png"));
-	public static readonly VAK = new Language(ELanguage.VAK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.vak.png"));
+
+	/**
+	 * Map of ELanguage to Language
+	 */
+	public static readonly Languages = new Map<ELanguage, Language>([
+		[ELanguage.ARAMAIC, new Language(ELanguage.ARAMAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.aramaic.png")],
+		[ELanguage.CRACKTRACK, new Language(ELanguage.CRACKTRACK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.cracktrack.png")],
+		[ELanguage.ERICAPAEAN, new Language(ELanguage.ERICAPAEAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ericapaean.png")],
+		[ELanguage.FUCINE, new Language(ELanguage.FUCINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.fucine.png")],
+		[ELanguage.GREEK, new Language(ELanguage.GREEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.greek.png")],
+		[ELanguage.HENAVEK, new Language(ELanguage.HENAVEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.henavek.png")],
+		[ELanguage.HYKSOS, new Language(ELanguage.HYKSOS, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.hyksos.png")],
+		[ELanguage.KILLASIMI, new Language(ELanguage.KILLASIMI, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.killasimi.png")],
+		[ELanguage.LATIN, new Language(ELanguage.LATIN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.latin.png")],
+		[ELanguage.MANDAIC, new Language(ELanguage.MANDAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.mandaic.png")],
+		[ELanguage.PHRYGIAN, new Language(ELanguage.PHRYGIAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.phrygian.png")],
+		[ELanguage.RAMSUND, new Language(ELanguage.RAMSUND, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ramsund.png")],
+		[ELanguage.SABAZINE, new Language(ELanguage.SABAZINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sabazine.png")],
+		[ELanguage.SANSKRIT, new Language(ELanguage.SANSKRIT, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sanskrit.png")],
+		[ELanguage.VAK, new Language(ELanguage.VAK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.vak.png")]
+	]);
+
+
 
 	/**
 	 * The enum object for this language.
