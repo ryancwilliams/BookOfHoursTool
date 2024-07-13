1606d68d4b1a8083201aeecc301d39cd2e9f156d
ryancwilliams <rcw4rcw@gmail.com>
2024-07-13 11:45:06 -0400

    Added Language Names

--- BookOfHours-Tool/src/model/language.enum.ts
+++ BookOfHours-Tool/src/model/language.enum.ts
@@ -33,24 +33,22 @@ export class Language {
 	 * Map of ELanguage to Language
 	 */
 	public static readonly Languages = new Map<ELanguage, Language>([
-		[ELanguage.ARAMAIC, new Language(ELanguage.ARAMAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.aramaic.png")],
-		[ELanguage.CRACKTRACK, new Language(ELanguage.CRACKTRACK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.cracktrack.png")],
-		[ELanguage.ERICAPAEAN, new Language(ELanguage.ERICAPAEAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ericapaean.png")],
-		[ELanguage.FUCINE, new Language(ELanguage.FUCINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.fucine.png")],
-		[ELanguage.GREEK, new Language(ELanguage.GREEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.greek.png")],
-		[ELanguage.HENAVEK, new Language(ELanguage.HENAVEK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.henavek.png")],
-		[ELanguage.HYKSOS, new Language(ELanguage.HYKSOS, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.hyksos.png")],
-		[ELanguage.KILLASIMI, new Language(ELanguage.KILLASIMI, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.killasimi.png")],
-		[ELanguage.LATIN, new Language(ELanguage.LATIN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.latin.png")],
-		[ELanguage.MANDAIC, new Language(ELanguage.MANDAIC, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.mandaic.png")],
-		[ELanguage.PHRYGIAN, new Language(ELanguage.PHRYGIAN, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.phrygian.png")],
-		[ELanguage.RAMSUND, new Language(ELanguage.RAMSUND, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ramsund.png")],
-		[ELanguage.SABAZINE, new Language(ELanguage.SABAZINE, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sabazine.png")],
-		[ELanguage.SANSKRIT, new Language(ELanguage.SANSKRIT, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sanskrit.png")],
-		[ELanguage.VAK, new Language(ELanguage.VAK, "", new Symbol(this.SYMBOL_PATH_PREFIX + "w.vak.png")]
+		[ELanguage.ARAMAIC, new Language(ELanguage.ARAMAIC, "Aramaic", new Symbol(this.SYMBOL_PATH_PREFIX + "w.aramaic.png")],
+		[ELanguage.CRACKTRACK, new Language(ELanguage.CRACKTRACK, "Cracktrack", new Symbol(this.SYMBOL_PATH_PREFIX + "w.cracktrack.png")],
+		[ELanguage.ERICAPAEAN, new Language(ELanguage.ERICAPAEAN, "Ericapaean", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ericapaean.png")],
+		[ELanguage.FUCINE, new Language(ELanguage.FUCINE, "Fucine", new Symbol(this.SYMBOL_PATH_PREFIX + "w.fucine.png")],
+		[ELanguage.GREEK, new Language(ELanguage.GREEK, "Greek", new Symbol(this.SYMBOL_PATH_PREFIX + "w.greek.png")],
+		[ELanguage.HENAVEK, new Language(ELanguage.HENAVEK, "Kernewek Henavek", new Symbol(this.SYMBOL_PATH_PREFIX + "w.henavek.png")],
+		[ELanguage.HYKSOS, new Language(ELanguage.HYKSOS, "Hyksos", new Symbol(this.SYMBOL_PATH_PREFIX + "w.hyksos.png")],
+		[ELanguage.KILLASIMI, new Language(ELanguage.KILLASIMI, "Killasimi", new Symbol(this.SYMBOL_PATH_PREFIX + "w.killasimi.png")],
+		[ELanguage.LATIN, new Language(ELanguage.LATIN, "Latin", new Symbol(this.SYMBOL_PATH_PREFIX + "w.latin.png")],
+		[ELanguage.MANDAIC, new Language(ELanguage.MANDAIC, "Deep Mandaic", new Symbol(this.SYMBOL_PATH_PREFIX + "w.mandaic.png")],
+		[ELanguage.PHRYGIAN, new Language(ELanguage.PHRYGIAN, "Phrygian", new Symbol(this.SYMBOL_PATH_PREFIX + "w.phrygian.png")],
+		[ELanguage.RAMSUND, new Language(ELanguage.RAMSUND, "Ramsund", new Symbol(this.SYMBOL_PATH_PREFIX + "w.ramsund.png")],
+		[ELanguage.SABAZINE, new Language(ELanguage.SABAZINE, "Sabazine", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sabazine.png")],
+		[ELanguage.SANSKRIT, new Language(ELanguage.SANSKRIT, "Sanskrit", new Symbol(this.SYMBOL_PATH_PREFIX + "w.sanskrit.png")],
+		[ELanguage.VAK, new Language(ELanguage.VAK, "Vak", new Symbol(this.SYMBOL_PATH_PREFIX + "w.vak.png")]
 	]);
-
-
 
 	/**
 	 * The enum object for this language.
