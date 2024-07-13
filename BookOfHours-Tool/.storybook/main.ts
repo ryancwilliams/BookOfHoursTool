3138a37c98e1e21774a6ec5aac7c412ed6ff5214
ryancwilliams <rcw4rcw@gmail.com>
2024-07-13 14:04:36 -0400

    Added Storybook

---
+++ BookOfHours-Tool/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/angular";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/angular",
+    options: {},
+  },
+	staticDirs: ['../public'],
+};
+export default config;
